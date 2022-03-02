385475958d8ed53efe3fc7b6c245e014f7a864ef
beinjamin <jauresbeinjamin@gmail.com>
2022-03-02 18:20:44 +0100

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
@@ -11,4 +12,20 @@ class PostsController < ApplicationController
    def edit 
     @post = Post.find(params[:id])
    end
+
+  
+
+   def update 
+    @post = Post.find(params[:id])
+    post_params =  params.require(:post).permit(:name,:content)
+    @post.update(post_params)
+
+    redirect_to posts_path
+
+  end
+
+  def new 
+    @post = post.new
+  end
+
 end
