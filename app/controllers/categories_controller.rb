74279ff8d3c267c646b9955a22396ae9780358b6
beinjamin <jauresbeinjamin@gmail.com>
2022-03-02 23:33:09 +0100

    Gestion du controlleur categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,19 @@
+class CategoriesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+ 
   
  
   root to: 'pages#home'
   get '/bonjour(/:name)',to:'pages#salut', as: 'salut'
   resources :posts
+  resources :categories
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
