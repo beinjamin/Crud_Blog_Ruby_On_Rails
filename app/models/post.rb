a7e9fb269965f931765c7758bcb1358b7a782a4b
beinjamin <jauresbeinjamin@gmail.com>
2022-03-07 19:49:56 +0100

    Gestion de la validation posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,17 @@
 class Post < ApplicationRecord 
 
+    validates :name, presence: true
+    
+
+
+
+
+
+
+    def as_json(options = nil)
+
+        super(only: [:name, :id, :created_at, :updated_at])
+    end
 
 
 
